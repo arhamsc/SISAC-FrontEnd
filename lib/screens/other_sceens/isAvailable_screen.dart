cda458e1ab7beee1ea918f71613d1456df429ca5
arhamsc <shaik.arham@gmail.com>
2022-01-04 22:37:04 +0530

    minor changes

---
+++ lib/screens/other_sceens/isAvailable_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../providers/cafetaria/cafataria_providers.dart';
+import '../../providers/cafetaria/restaurant_providers.dart';
+
+import '../../../widgets/app_bar.dart';
+import '../../widgets/cafetaria/restaurant/isAvailable_cards.dart';
+import '../../../widgets/cafetaria/bottom_nav.dart';
+import '../../widgets/cafetaria/restaurant/isAvailable_cards.dart';
+
+import '../../../utils/helpers/error_dialog.dart';
+import '../../../utils/general/screen_size.dart';
+
+class IsAvailableScreen extends StatefulWidget {
+  const IsAvailableScreen({Key? key}) : super(key: key);
+  static const routeName = '/cafetaria/restaurant/isAvailable';
+
+  @override
+  State<IsAvailableScreen> createState() => _IsAvailableScreenState();
+}
+
+class _IsAvailableScreenState extends State<IsAvailableScreen> {
+  var _expanded = false;
+
+  Future<void> _refreshItems(BuildContext context) async {
+    return await Provider.of<MenuItemProvider>(context, listen: false)
+        .fetchMenu();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: BaseAppBar.getAppBar(
+        title: "Cafetaria",
+        context: context,
+        subtitle: "Menu Available",
+        // TODO: Implement time range
+      ),
+      body: FutureBuilder(
+        future:
+            Provider.of<MenuItemProvider>(context, listen: false).fetchMenu(),
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (dataSnapShot.error != null) {
+            Future.delayed(
+              Duration.zero,
+              () => dialog(
+                ctx: context,
+                errorMessage: dataSnapShot.error.toString(),
+                tryAgainFunc: () => _refreshItems(context),
+                pop2Pages: true,
+              ),
+            );
+            return RefreshIndicator(
+              onRefresh: () => _refreshItems(context),
+              child: const Center(
+                child: Text("Error"),
+              ),
+            );
+          } else {
+            return RefreshIndicator(
+              onRefresh: () => _refreshItems(context),
+              child: Column(
+                children: [
+                  Consumer<MenuItemProvider>(
+                    builder: (ctx, menuData, child) => Container(
+                      child: SingleChildScrollView(
+                        child: Container(
+                          height: ScreenSize.usableHeight(context),
+                          child: ListView.builder(
+                            itemBuilder: (ctx, i) => IsAvailableCard(
+                              menu: menuData.items[i],
+                              setFunc: () {
+                                setState(() {});
+                              },
+                              key: Key(menuData.items[i].id),
+                            ),
+                            itemCount: menuData.items.length,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: BottomNav(
+                      isSelected: "Cafetaria",
+                      showOnlyOne: true,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/cafetaria/restaurant/isAvailable_cards.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:sisac/utils/helpers/http_exception.dart';
+import 'package:provider/provider.dart';
+
+import '../../../providers/cafetaria/cafataria_providers.dart';
+import '../../../providers/cafetaria/restaurant_providers.dart';
+import '../../../providers/user_provider.dart';
+
+import '../../../utils/general/screen_size.dart';
+import '../../../utils/general/customColor.dart';
+
+import '../../../utils/helpers/error_dialog.dart';
+
+class IsAvailableCard extends StatefulWidget {
+  IsAvailableCard({Key? key, required this.menu, required this.setFunc})
+      : super(key: key);
+
+  final MenuItem menu;
+  final Function setFunc;
+
+  @override
+  _IsAvailableCardState createState() => _IsAvailableCardState();
+}
+
+class _IsAvailableCardState extends State<IsAvailableCard> {
+  bool _isLoading = false;
+
+  bool get presentIsAvailable {
+    return widget.menu.isAvailable;
+  }
+
+  Future<void> updateMenu(MenuItemProvider menuItemP, String id) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      var res = await menuItemP.updateIsAvailable(id, widget.menu.isAvailable);
+      await dialog(
+          ctx: context, errorMessage: "Menu Updated", title: "Success");
+
+      setState(() {
+        _isLoading = false;
+      });
+    } on HttpException catch (error) {
+      await dialog(ctx: context, errorMessage: error.message);
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final menuP = Provider.of<MenuItemProvider>(context);
+    return Column(
+      key: widget.key,
+      children: [
+        const SizedBox(height: 25),
+        Center(
+          child: Container(
+            height: ScreenSize.screenHeight(context) * .12,
+            width: ScreenSize.screenWidth(context) * .85,
+            decoration: BoxDecoration(
+                color: SecondaryPallete.primary,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 6,
+                    color: Colors.black54,
+                    spreadRadius: 2,
+                    offset: Offset(0, -2),
+                  ),
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: ScreenSize.screenWidth(context) * .25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(widget.menu.imageUrl),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        widget.menu.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5!
+                            .copyWith(fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        widget.menu.isAvailable ? 'Available' : 'Not Available',
+                        style: Theme.of(context).textTheme.headline5!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                              color: Palette.tertiaryDefault,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(),
+                    Row(
+                      children: [
+                        Container(
+                          height: ScreenSize.screenHeight(context) * 0.04,
+                          width: ScreenSize.screenWidth(context) * .08,
+                          child: Center(
+                            child: Switch(
+                              value: widget.menu.isAvailable,
+                              onChanged: (_) async {
+                                await updateMenu(
+                                  menuP,
+                                  widget.menu.id,
+                                );
+                                //print(widget.menu.id);
+                                widget.setFunc();
+                              },
+                              activeTrackColor: Palette.quaternaryDefault,
+                              inactiveTrackColor: SecondaryPallete.quaternary,
+                              inactiveThumbColor: Palette.tertiaryDefault,
+                              activeColor: Palette.quinaryDefault,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
